82ae6b332ebbfa8622fcaa7e4d307acfbe25c175
Dani Toledo <79161140+Dani-24@users.noreply.github.com>
2023-10-03 12:11:26 +0200

    Prototyping v0.1

--- Multiplayer_Prototype/Assets/Scripts/PlayerMovement.cs
+++ Multiplayer_Prototype/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,20 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
 
+    [SerializeField]
+    private Vector2 movementInput;
+
+    [SerializeField]
+    private float playerSpeed = 1.0f;
+
+    [SerializeField]
+    private bool jumped = false;
+
+    private float speedY;
+
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+
     void Start()
     {
         controller = GetComponentInChildren<CharacterController>();
@@ -12,11 +26,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        // =============== Movement
+
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    
+
+        // =============== Jump
+
+        if(jumped)
+        {
+            speedY += Mathf.Sqrt(jumpHeight * -3.0f * -9.81f);
+        }
+
+        speedY += -9.81f * Time.deltaTime;
+        controller.SimpleMove(new Vector3(0,speedY * Time.deltaTime,0));
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        
+        movementInput = new Vector2(context.ReadValue<Vector2>().x, context.ReadValue<Vector2>().y);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jumped = context.action.triggered;
     }
 }
