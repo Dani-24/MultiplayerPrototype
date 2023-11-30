7f61f2f65c5fe49c4df2fc47ada23b81af341654
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2023-11-30 21:41:53 +0100

    Block Player Inputs on settings/Title

--- MultiplayerGame/Assets/Scripts/Managers/CameraManager.cs
+++ MultiplayerGame/Assets/Scripts/Managers/CameraManager.cs
@@ -70,7 +70,7 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        if (currentCam == playerCamera && !brain.IsBlending)
+        if (currentCam == playerCamera && !brain.IsBlending && SceneManagerScript.Instance.gameState == SceneManagerScript.GameState.Gameplay)
         {
             SceneManagerScript.Instance.GetOwnPlayerInstance().GetComponent<PlayerStats>().playerInputEnabled = true;
         }
