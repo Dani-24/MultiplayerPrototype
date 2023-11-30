d5d6a6859d7bf4794ad641e328c59b4cffd52d1c
Dani Toledo <79161140+Dani-24@users.noreply.github.com>
2023-11-30 12:25:49 +0100

    Cameras clean organization

--- MultiplayerGame/Assets/Scripts/Managers/CameraManager.cs
+++ MultiplayerGame/Assets/Scripts/Managers/CameraManager.cs
@@ -1,57 +1,20 @@
 using UnityEngine;
+using Cinemachine;
+using System.Collections.Generic;
 
 public class CameraManager : MonoBehaviour
 {
-    //[SerializeField] Camera titleCamera;
-    //[SerializeField] Camera mapCamera;
+    [SerializeField] List<CinemachineVirtualCamera> sceneCameras = new List<CinemachineVirtualCamera>();
 
-    //bool cameraMissing = false;
+    public static CinemachineVirtualCamera activeCamera;
 
     void Start()
     {
-        //try
-        //{
-        //    titleCamera = GameObject.FindGameObjectWithTag("TitleCam").GetComponent<Camera>();
-        //    mapCamera = GameObject.FindGameObjectWithTag("MapCam").GetComponent<Camera>();
-        //}
-        //catch
-        //{
-        //    Debug.Log("Some Camera is missing");
-        //    cameraMissing = true;
-        //}
+        
     }
 
     void Update()
     {
-        //if (!cameraMissing)
-        //{
-        //    switch (SceneManagerScript.Instance.gameState)
-        //    {
-        //        case SceneManagerScript.GameState.Title:
-        //            titleCamera.gameObject.SetActive(true);
-        //            mapCamera.gameObject.SetActive(false);
 
-        //            // Don't show UIs on UIs
-        //            //GetComponent<UI_Manager>().showUI = false;
-        //            break;
-        //        case SceneManagerScript.GameState.Gameplay:
-        //            titleCamera.gameObject.SetActive(false);
-        //            mapCamera.gameObject.SetActive(true);
-
-        //            break;
-        //    }
-        //}
-    }
-
-    private void LateUpdate()
-    {
-        //if (SceneManagerScript.Instance.playersOnScene.Count == 0)
-        //{
-        //    SceneManagerScript.Instance.gameState = SceneManagerScript.GameState.Title;
-        //}
-        //else if (Camera.main != null)
-        //{
-        //    SceneManagerScript.Instance.gameState = SceneManagerScript.GameState.Gameplay;
-        //}
     }
 }
